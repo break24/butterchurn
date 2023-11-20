759363da2113072573b99bdfa08cbf1334dfe3c5
steffenkrueger <mail@steffenkrueger.de>
2023-11-20 22:57:10 +0100

    added create test presets

---
+++ preset-docs/presets.js
@@ -0,0 +1,8 @@
+/* eslint-disable max-len */
+const presetFolder = ".";
+const presets = {
+  "test-1": require(`${presetFolder}/json/test-1.json`),
+  "test-2": require(`${presetFolder}/json/test-2.json`),
+};
+
+export default presets;
---
+++ preset-docs/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require("path");
+
+const root = path.join(__dirname, ".");
+const nodeRoot = path.join(__dirname,"..", "node_modules");
+const outputPath = path.join(__dirname, "presets");
+
+const config = {
+  entry: {
+    base: root + "/presets.js",
+  },
+  output: {
+    path: outputPath,
+    filename: "[name]",
+    library: "[name]ButterchurnPresets",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /(\.js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader?cacheDirectory",
+          options: {
+            plugins: ["@babel/plugin-transform-runtime"],
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
+  resolve: {
+    modules: [root, nodeRoot],
+    extensions: [".js"],
+  },
+};
+
+module.exports = (env, argv) => {
+  if (argv.mode === "development") {
+    config.devtool = "source-map";
+  }
+
+  if (argv.mode === "production") {
+    config.output.filename += ".min";
+  }
+
+  config.output.filename += ".js";
+
+  return config;
+};
